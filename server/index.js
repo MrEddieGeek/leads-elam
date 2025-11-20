3edccfb0c3641d9dcfdc82a3bbd145624b240550
MrEddieGeek <suscripcioneseddie@gmail.com>
2025-11-20 15:48:08 -0600

    Fix final: servir ../dist en vez de client/dist

--- server/index.js
+++ server/index.js
@@ -35,7 +35,8 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('*', (req, res) => {
+
+  app.get(/^(?!\/api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
